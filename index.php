fbc6b6c22faeeeb963bc8fe797a7439829468bdf
ginarose <ginapiscitelli@gmail.com>
2014-09-03 17:50:19 -0400

    I don't know where this is going

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<link rel="stylesheet" href="jquerymobile_theme/themes/roccos_records.min.css" />
+<link rel="stylesheet" href="jquerymobile_theme/themes/jquery.mobile.icons.min.css" />
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.3/jquery.mobile.structure-1.4.3.min.css" />
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.3/jquery.mobile-1.4.3.min.js"></script>
+ 
+	<meta charset="UTF-8">
+	<title>Project 1</title>
+</head>
+
+<!-- HOMEPAGE -->
+<div data-role="page" id="home">
+
+	<div data-role="header">
+		<img src="assets/record.jpg">
+		<h1>Rocco's Records</h1>
+	</div><!-- /header -->
+
+	<div data-role="content">	
+		<h2>Welcome to Rocco's Records!</h2>
+		<p>Here at Rocco's we spin nothing but the latest and greatest (and okay, maybe some old jams too) and have the lowest prices in town. We pride ourself on customer service and fair prices. Since our records fly off the shelves, we don't have an online catalog, but feel free to contact us to ask about your favorite album and check if it's in stock.</p>
+
+		<p><a href="#about">Learn More...</a></p>	
+	</div><!-- /content -->
+
+	<div data-role="footer">
+		<h4>&copy; 2004 - 2014, Rocco Romaine. All Rights Reserved.</h4>
+	</div><!-- /footer -->
+</div><!-- /page -->
+
+
+<!-- About page -->
+<div data-role="page" id="about">
+
+	<div data-role="header">
+		<img src="assets/record.jpg">
+		<h1>About Rocco's</h1>
+	</div><!-- /header -->
+
+	<div data-role="content">	
+		<p>Rocco's was started in 2004 by our very own Rocco Romaine. A Florida native with a passion for music, Rocco wanted to keep the local scene alive and opened up Rocco's Records as a place for bands to play for the musically appreciative residents of Winter Park, FL.</p>	
+		<p>Today, Rocco not only manages the "Live Jive Corner," a section of Rocco's Records where bands still come to play their melodies, but he has successfully offered a consignment system for records.</p>
+		<p></p>
+
+		<p><a href="#home">Back Home</a></p>	
+	</div><!-- /content -->
+
+	<div data-role="footer">
+		<h4>&copy; 2004 - 2014, Rocco Romaine. All Rights Reserved.</h4>
+	</div><!-- /footer -->
+</div><!-- /page -->
+
+
+	
+</body>
+</html>
