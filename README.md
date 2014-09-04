0b60c35dd311ef8aae35ecb0cde6e2d0d218e1e3
ginarose <ginapiscitelli@gmail.com>
2014-09-03 20:25:05 -0400

    Navigation added, home and about pages are up and running.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 RoccosRecords
 =============
 
-A jquerymobile site for an imaginary business
+A jquerymobile site for an imaginary business. Some icons are from <a href="http://glyphish.com/">Glyphish</a>.
--- index.php
+++ index.php
@@ -21,6 +21,16 @@
 		<h1>Rocco's Records</h1>
 	</div><!-- /header -->
 
+	<div data-role="navbar">
+	<ul>
+		<li><a href="#home" class="ui-btn-active">Home</a></li>
+		<li><a href="#about">About</a></li>
+		<li><a href="#gallery">Gallery</a></li>
+		<li><a href="#events">Events</a></li>
+		<li><a href="#contact">Contact</a></li>
+	</ul>
+	</div><!-- /navbar -->
+
 	<div data-role="content">	
 		<h2>Welcome to Rocco's Records!</h2>
 		<p>Here at Rocco's we spin nothing but the latest and greatest (and okay, maybe some old jams too) and have the lowest prices in town. We pride ourself on customer service and fair prices. Since our records fly off the shelves, we don't have an online catalog, but feel free to contact us to ask about your favorite album and check if it's in stock.</p>
@@ -41,6 +51,16 @@
 		<img src="assets/logo.png" alt="Rocco's Records logo" class="header-image">
 		<h1>About Rocco's</h1>
 	</div><!-- /header -->
+
+	<div data-role="navbar">
+	<ul>
+		<li><a href="#home">Home</a></li>
+		<li><a href="#about" class="ui-btn-active">About</a></li>
+		<li><a href="#gallery">Gallery</a></li>
+		<li><a href="#events">Events</a></li>
+		<li><a href="#contact">Contact</a></li>
+	</ul>
+	</div><!-- /navbar -->
 
 	<div data-role="content">	
 		<p>Rocco's was started in 2004 by our very own Rocco Romaine. A Florida native with a passion for music, Rocco wanted to keep the local scene alive and opened up Rocco's Records as a place for bands to play for the musically appreciative residents of Winter Park, FL.</p>	
