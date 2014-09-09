ad1cfc465f5411ef249d8f8293f6e21d9b44b719
ginarose <ginapiscitelli@gmail.com>
2014-09-08 21:19:40 -0400

    Moved nav bar around, added icons, edited front page

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.4.3/jquery.mobile-1.4.3.min.js"></script>
 <link rel="stylesheet" href="assets/css/styling.css" />
-
+<script src="assets/script.js"></script>
 <script src="swipebox/lib/jquery-2.0.3.js"></script>
 <script src="swipebox/src/js/jquery.swipebox.js"></script>
 <link rel="stylesheet" href="swipebox/src/css/swipebox.css">
@@ -25,21 +25,27 @@
 		<h1>Rocco's Records</h1>
 	</div><!-- /header -->
 
-	<div data-role="navbar">
-	<ul>
-		<li><a href="#home" class="ui-btn-active">Home</a></li>
-		<li><a href="#about">About</a></li>
-		<li><a href="#gallery">Gallery</a></li>
-		<li><a href="#events">Events</a></li>
-		<li><a href="#contact">Contact</a></li>
-	</ul>
-	</div><!-- /navbar -->
+		<div data-role="navbar" data-iconpos="top">
+		<ul>
+			<li><a href="#home" data-icon="home" class="ui-btn-active">Home</a></li>
+			<li><a href="#gallery" data-icon="grid">Gallery</a></li>
+			<li><a href="#events" data-icon="calendar">Events</a></li>
+		</ul>
+		</div><!-- /navbar -->
 
 	<div data-role="content">	
-		<h2>Welcome to Rocco's Records!</h2>
-		<p>Here at Rocco's we spin nothing but the latest and greatest (and okay, maybe some old jams too) and have the lowest prices in town. We pride ourself on customer service and fair prices. Since our records fly off the shelves, we don't have an online catalog, but feel free to contact us to ask about your favorite album and check if it's in stock.</p>
+		<div data-role="collapsible" data-collapsed="false">
+		    <h4>Welcome to Rocco's Records!</h4>
+		    	<p>Here at Rocco's we spin nothing but the latest and greatest (and okay, maybe some old jams too) and have the lowest prices in town. We pride ourself on customer service and fair prices. Since our records fly off the shelves, we don't have an online catalog, but feel free to contact us to ask about your favorite album and check if it's in stock.</p>
+		</div>	
 
-		<p><a href="#about">Learn More...</a></p>	
+		<div id="map-canvas"></div>
+
+		<div data-role="collapsible">
+		    <h4>The History of Rocco's</h4>
+		    	<p>Rocco's was started in 2004 by our very own Rocco Romaine. A Florida native with a passion for music, Rocco wanted to keep the local scene alive and opened up Rocco's Records as a place for bands to play for the musically appreciative residents of Winter Park, FL.</p>	
+				<p>Today, Rocco not only manages the "Live Jive Corner," a section of Rocco's Records where bands still come to play their melodies, but he has successfully offered a consignment system for records.</p>
+		</div>
 	</div><!-- /content -->
 
 	<div data-role="footer">
@@ -47,38 +53,6 @@
 	</div><!-- /footer -->
 </div><!-- /page -->
 
-
-<!-- About page -->
-<div data-role="page" id="about">
-
-	<div data-role="header">
-		<img src="assets/logo.png" alt="Rocco's Records logo" class="header-image">
-		<h1>About Rocco's</h1>
-	</div><!-- /header -->
-
-	<div data-role="navbar">
-	<ul>
-		<li><a href="#home">Home</a></li>
-		<li><a href="#about" class="ui-btn-active">About</a></li>
-		<li><a href="#gallery">Gallery</a></li>
-		<li><a href="#events">Events</a></li>
-		<li><a href="#contact">Contact</a></li>
-	</ul>
-	</div><!-- /navbar -->
-
-	<div data-role="content">	
-		<h2>More about Rocco's</h2>
-		<p>Rocco's was started in 2004 by our very own Rocco Romaine. A Florida native with a passion for music, Rocco wanted to keep the local scene alive and opened up Rocco's Records as a place for bands to play for the musically appreciative residents of Winter Park, FL.</p>	
-		<p>Today, Rocco not only manages the "Live Jive Corner," a section of Rocco's Records where bands still come to play their melodies, but he has successfully offered a consignment system for records.</p>
-		<p></p>
-
-		<p><a href="#home">Back Home</a></p>	
-	</div><!-- /content -->
-
-	<div data-role="footer">
-		<h4>&copy; 2004 - 2014, Rocco Romaine. All Rights Reserved.</h4>
-	</div><!-- /footer -->
-</div><!-- /page -->
 
 <!-- Gallery page -->
 <div data-role="page" id="gallery">
@@ -88,13 +62,11 @@
 		<h1>Take a peek</h1>
 	</div><!-- /header -->
 
-	<div data-role="navbar">
+	<div data-role="navbar" data-iconpos="top">
 	<ul>
-		<li><a href="#home">Home</a></li>
-		<li><a href="#about">About</a></li>
-		<li><a href="#gallery" class="ui-btn-active">Gallery</a></li>
-		<li><a href="#events">Events</a></li>
-		<li><a href="#contact">Contact</a></li>
+		<li><a href="#home" data-icon="home" >Home</a></li>
+		<li><a href="#gallery" data-icon="grid" class="ui-btn-active">Gallery</a></li>
+		<li><a href="#events" data-icon="calendar">Events</a></li>
 	</ul>
 	</div><!-- /navbar -->
 
@@ -118,11 +90,13 @@
 
 
 <script type="text/javascript">
+//swipebox functionality
 ;( function( $ ) {
 
 	$( '.swipebox' ).swipebox();
 
 } )( jQuery );
+
 </script>
 
 </body>
